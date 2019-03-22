3e2d50edf2c04873262334598802c4d1007f71d4
johnSickels <46609057+johnsickels@users.noreply.github.com>
2019-03-22 15:25:34 -0400

    javascript grade answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,13 +7,13 @@ $(document).ready(function () {
     var unanswered = "";
     var intervalId;
 
-    $(".questions").hide();
-    
+    $("#questions").hide();
+
     $("#start").on("click", run);
     $("#submit").on("click", stop);
 
     function run() {
-        $(".questions").show();
+        $("#questions").show();
         $("#content").html("<h2>Time Remaining: " + timeLeft + " Seconds</h2>");
         clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
@@ -30,9 +30,94 @@ $(document).ready(function () {
 
     function stop() {
         clearInterval(intervalId);
-        $(".questions").hide();
-        $("#content").html("<h2>All done</h2><h3>Correct: " + correct + "</h3><h3>Inccorect: " + incorrect + "</h3><h3>Unanswered: " + unanswered)
-    };
+        $("#questions").hide();
+        if ($('input[name="inlineRadioOptions"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions2"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions2"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions3"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions3"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions4"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions4"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions5"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions5"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions6"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions6"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions7"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions7"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions8"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions8"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions9"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions9"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions10"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions10"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions11"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions11"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        if ($('input[name="inlineRadioOptions12"]:checked').val() === "y") {
+            correct++;
+        } else if ($('input[name="inlineRadioOptions12"]:checked').val() === "n") {
+            incorrect++;
+        } else {
+            unanswered++;
+        };
+        
+        $("#content").html("<h2>All done</h2><h3>Correct: " + correct + "</h3><h3>Incorrect: " + incorrect + "</h3><h3>Unanswered: " + unanswered);
 
+    };
 
 });
